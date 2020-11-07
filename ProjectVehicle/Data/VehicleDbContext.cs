ff5586a06a230187bd338694c75130e46841d27c
Paleozoik <Paleozoik007@gmail.com>
2020-11-07 08:17:26 +0100

    Stvorene klase, DbContext i baza podataka

---
+++ ProjectVehicle/Data/VehicleDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectVehicle.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectVehicle.Data
+{
+    public class VehicleDbContext : DbContext
+    {
+        public VehicleDbContext(DbContextOptions<VehicleDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<VehicleMake> VehicleMake { get; set; }
+        public DbSet<VehicleModel> VehicleModel { get; set; }
+    }
+}
--- ProjectVehicle/Startup.cs
+++ ProjectVehicle/Startup.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProjectVehicle.Data;
 
 namespace ProjectVehicle
 {
@@ -22,6 +24,8 @@ namespace ProjectVehicle
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VehicleDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("LocalSqlConnection")));
+
             services.AddControllersWithViews();
         }
 
