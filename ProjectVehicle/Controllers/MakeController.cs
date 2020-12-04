44afab1e2497fe5a1f085474b3c8b32a4eb59684
Paleozoik <Paleozoik007@gmail.com>
2020-12-04 05:12:19 +0100

    Razdvojen VehicleController na Make i ModelController

---
+++ ProjectVehicle/Controllers/MakeController.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Service.DTOs;
+using Service.Interfaces;
+using Service.Models;
+using Service.Paging;
+using System;
+using System.Threading.Tasks;
+
+namespace ProjectVehicle.Controllers
+{
+    public class MakeController : Controller
+    {
+        private IVehicleWrapper _wrapper;
+        private IMapper _mapper;
+
+        public MakeController(IVehicleWrapper wrapper, IMapper mapper)
+        {
+            _wrapper = wrapper;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index(MakePaging pagingParams)
+        {
+            ViewData["sortBy"] = String.IsNullOrEmpty(pagingParams.SortBy) ? "MakeD" : "";
+
+            var PagedMake = await _wrapper.Make.GetMakesAsync(pagingParams);
+            var PagedDTO = _mapper.Map<PagedList<MakeDTO>>(PagedMake);
+
+            PagedDTO.CurrentPage = PagedMake.CurrentPage;
+            PagedDTO.PageSize = PagedMake.PageSize;
+            PagedDTO.TotalCount = PagedMake.TotalCount;
+            PagedDTO.TotalPages = PagedMake.TotalPages;
+
+            ViewBag.Data = PagedDTO;
+            return View(pagingParams);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateMakeDTO make)
+        {
+            if (ModelState.IsValid)
+            {
+                await _wrapper.Make.CreateMakeAsync(_mapper.Map<VehicleMake>(make));
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var make = await _wrapper.Make.GetMakeByIdAsync(id);
+            if (make == null)
+            {
+                return BadRequest("Invalid ID");
+            }
+            return View(_mapper.Map<EditMakeDTO>(make));
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(EditMakeDTO make)
+        {
+            if (ModelState.IsValid)
+            {
+                await _wrapper.Make.UpdateMakeAsync(_mapper.Map<VehicleMake>(make));
+                return RedirectToAction("Index");
+            }
+            return View(make.Id);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            VehicleMake make = await _wrapper.Make.GetMakeByIdAsync(id);
+            if (make == null)
+            {
+                return BadRequest("Invalid ID");
+            }
+            await _wrapper.Make.DeleteMakeAsync(make);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ProjectVehicle/Controllers/ModelController.cs
@@ -0,0 +1,101 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Service.DTOs;
+using Service.Interfaces;
+using Service.Models;
+using Service.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectVehicle.Controllers
+{
+    public class ModelController : Controller
+    {
+        private IVehicleWrapper _wrapper;
+        private IMapper _mapper;
+
+        public ModelController(IVehicleWrapper wrapper, IMapper mapper)
+        {
+            _wrapper = wrapper;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index(ModelPaging pagingParams)
+        {
+            ViewData["sortByModel"] = String.IsNullOrEmpty(pagingParams.SortBy) ? "ModelD" : "";
+            ViewData["sortByMake"] = pagingParams.SortBy == "MakeA" ? "MakeD" : "MakeA";
+
+            var PagedModel = await _wrapper.Model.GetModelsAsync(pagingParams);
+            var PagedDTO = _mapper.Map<PagedList<ModelDTO>>(PagedModel);
+
+            PagedDTO.CurrentPage = PagedModel.CurrentPage;
+            PagedDTO.PageSize = PagedModel.PageSize;
+            PagedDTO.TotalCount = PagedModel.TotalCount;
+            PagedDTO.TotalPages = PagedModel.TotalPages;
+
+            ViewBag.Data = PagedDTO;
+            return View(pagingParams);
+        }
+        public async Task<IActionResult> Create()
+        {
+            var makes = await _wrapper.Make.GetAllMakesAsync();
+
+            ViewBag.SelectList = makes.Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            });
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateModelDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _wrapper.Model.CreateModelAsync(_mapper.Map<VehicleModel>(model));
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var makes = await _wrapper.Make.GetAllMakesAsync();
+            ViewBag.SelectList = makes.Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            });
+            var model = await _wrapper.Model.GetModelByIdAsync(id);
+            if (model == null)
+            {
+                return BadRequest("Invalid ID");
+            }
+            return View(_mapper.Map<EditModelDTO>(model));
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(EditModelDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _wrapper.Model.UpdateModelAsync(_mapper.Map<VehicleModel>(model));
+                return RedirectToAction("Index");
+            }
+            return View(model.Id);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            VehicleModel model = await _wrapper.Model.GetModelByIdAsync(id);
+            if (model == null)
+            {
+                return BadRequest("Invalid ID");
+            }
+            await _wrapper.Model.DeleteModelAsync(model);
+            return RedirectToAction("Index");
+        }
+    }
+}
